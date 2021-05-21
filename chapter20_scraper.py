de4685c1e67d39bbb915615109bfb07504450574
RooibosGood <takai3mobile@gmail.com>
2021-05-21 15:23:44 +0900

    Scraper for Yomiuri.

--- chapter20_scraper.py
+++ chapter20_scraper.py
@@ -5,6 +5,7 @@ Created on Fri May 21 11:04:55 2021
 @author: Takai
 """
 
+import re
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -17,12 +18,28 @@ class Scraper:
         html = r.read()
         parser = "html.parser"
         sp = BeautifulSoup(html, parser)
-        for tag in sp.find("a"):
+      #  print(html)
+        url_list =[]
+        match_list =["economy", "medical", "national"]
+        for tag in sp.find_all("a"):
+#            print(tag)
             url = tag.get("href")
+#            print(tag)
             if url is None:
                 continue
-            if "html" in url:
-                print("\n" + url)
+            if "medical" in url:
+                if "http" in url:
+                    r_sub = urllib.request.urlopen(url)
+                    html_sub = r_sub.read()
+                    sp_sub = BeautifulSoup(html_sub, parser)
+                    for tag_sub in sp_sub.find("title"):
+                        if url not in url_list:
+                            url_list.append(url)
+                            print(tag_sub)
+#                        print(url)
+#                print("\n" + url)
                 
-news = "https://news.google.com/"
+#news = 'https://news.google.com/'
+news = 'https://www.yomiuri.co.jp/'
+#news = 'https://www.yahoo.co.jp/'
 Scraper(news).scrape()
