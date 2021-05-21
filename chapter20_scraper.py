a7cfcc1d190e9816e245fea0b3365d00d351a7a4
RooibosGood <takai3mobile@gmail.com>
2021-05-21 11:17:15 +0900

    Modifying scraper.

--- chapter20_scraper.py
+++ chapter20_scraper.py
@@ -13,4 +13,16 @@ class Scraper:
         self.site = site
     
     def scrape(self):
-        pass
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find("a"):
+            url = tag.get("href")
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n" + url)
+                
+news = "https://news.google.com/"
+Scraper(news).scrape()
