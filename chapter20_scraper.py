37483ab70c67751bf84254aafed6b146a4a060d1
RooibosGood <84549638+RooibosGood@users.noreply.github.com>
2021-05-21 11:08:44 +0900

    Adding scraper.py.

---
+++ chapter20_scraper.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 11:04:55 2021
+
+@author: Takai
+"""
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+    
+    def scrape(self):
+        pass
